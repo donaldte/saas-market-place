f62c8f8a37fcc9d61b241ef0b6476e9aa0f8ab66
donaldte <donaldtedom0@gmail.com>
2024-07-28 10:49:14 +0100

    feat: initializing the project with cookiecutter

---
+++ marketplace/marketplace/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from marketplace.users.models import User
+from marketplace.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
